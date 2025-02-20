7f200fc2e03ebf8ec0922384dbf182eb0c6f9251
quarkusio <no-reply@quarkus.io>
2025-02-20 10:33:36 +0000

    Initial commit

---
+++ src/native-test/java/org/polytech/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.polytech;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
