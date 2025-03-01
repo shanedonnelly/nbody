1fb544c21fcfa7189a9deae39db1dbfa4f4d9b05
CorentinDelclaud <corentin.delclaud@gmail.com>
2025-03-01 18:39:02 +0100

    yes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 import io.quarkus.runtime.Quarkus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import simulation.SimulationTest;
 import simulation.SimulationLogic;
 
 
@@ -12,8 +11,6 @@ import simulation.SimulationLogic;
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
-    private static SimulationLogic simulationLogic;
 
     public static void main(String... args) {
         Quarkus.run(MyApp.class, args);
@@ -24,7 +21,7 @@ public class Main {
         @Override
         public int run(String... args) throws Exception {
             logger.info("Running main method");
-            simulationLogic = SimulationLogic.getInstance();
+            SimulationLogic simulationLogic = SimulationLogic.getInstance();
             simulationLogic.startSimulation();
             logger.info("Simulation started");
             Quarkus.waitForExit();
